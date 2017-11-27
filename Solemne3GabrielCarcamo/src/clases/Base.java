b1055732e5b8a5d04e9b6759ec39739747f27102
GABRIEL ALEJANDRO CARCAMO MARAMBIO <172493726@mayor.cl>
2017-11-27 20:34:37 -0300

    asd

---
+++ Solemne3GabrielCarcamo/src/clases/Base.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+/**
+ * Pendiente:  K: Clave de tipo genérico.....
+ * Clave: Rut
+ * @author 85605410
+ */
+public class Base{
+    private int key;  
+    private int info;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+     
+    public Base() {
+        this.key = 0;
+        this.info = 0;
+    }
+    
+    public Base(int key, int info) {
+        this.key = key;
+        this.info = info;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder(); 
+        s.append("key: ");
+        s.append(Integer.toString(this.getKey()));
+        s.append(" info: ");
+        s.append(Integer.toString(this.getInfo()));
+        s.append("\n");
+        return s.toString();
+    }
+    
+    public boolean infoIs(int esto){
+        return esto == this.key;
+    }
+}
---
+++ Solemne3GabrielCarcamo/src/clases/Nodo.java
@@ -0,0 +1,57 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *Representa el nodo de un ABB
+ * @author 85605410
+ */
+public class Nodo {
+    private Base dato;
+    private Nodo izq;
+    private Nodo der;
+    
+    public Nodo(){
+        dato=null;
+        izq=null;
+        der=null;
+    }
+    public Nodo(Base e){
+        dato = e;
+        izq=null;
+        der=null;
+    }
+    
+    public String toString(){
+        return dato.toString();
+    }
+
+    public Base getDato() {
+        return dato;
+    }
+
+    public void setDato(Base dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getIzq() {
+        return izq;
+    }
+
+    public void setIzq(Nodo izq) {
+        this.izq = izq;
+    }
+
+    public Nodo getDer() {
+        return der;
+    }
+
+    public void setDer(Nodo der) {
+        this.der = der;
+    }
+    
+    
+}
